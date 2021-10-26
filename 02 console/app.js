e47cb6871f1456e18088b96ad9d4329c1471d222
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-10-26 07:12:31 -0700

    Updated

---
+++ 02 console/app.js
@@ -0,0 +1,25 @@
+console.log('Hello')
+console.log(123)
+console.log(true)
+var message = 'This is a new message'
+console.log(message)
+console.log([1, 2, 3])
+console.log({
+    name: 'John',
+    age: 18
+})
+console.table({
+    name: 'John',
+    age: 18
+})
+console.error("Error!!")
+console.warn("Warning!!")
+console.clear()
+console.time('time')
+console.log('timing')
+console.log('timing')
+console.log('timing')
+console.log('timing')
+console.log('timing')
+console.log('timing')
+console.timeEnd('time')
