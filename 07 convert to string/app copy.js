a72993cfb7b3d36ff171fb7e18a43cfbd91e6dc6
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-10-28 20:37:33 +0500

    Updated

---
+++ 07 convert to string/app copy.js
@@ -0,0 +1,23 @@
+let val;
+//number to string
+val = 999
+val = String(val)
+
+//boolean to string
+val = true
+val = String(val)
+
+//date to string
+val = new Date()
+val = val.toString()
+//Array to string
+
+val = [1, 2, 3]
+val = val.toString()
+
+
+
+
+console.log(val)
+console.log(typeof val)
+console.log(val.length)
---
+++ 07 convert to string/app.js
@@ -0,0 +1,14 @@
+
+//String to number
+
+//Boolean to number
+
+
+//both false and null is 0
+
+
+
+
+console.log(val)
+console.log(typeof val)
+console.log(val.toFixed(2))
