2fabbcecf38c6ca8a2c194f824bf06bb4dff87c5
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-10-29 18:28:47 +0500

    Updated

---
+++ 08 convert to number/app.js
@@ -0,0 +1,20 @@
+let val
+//String to number
+val = '8'
+val = Number(val)
+//Boolean to number
+val = false
+val = null
+val = Number(val)
+
+//both false and null is 0
+val = 'Alan'
+val = [1, 2, 3]
+val = Number(val)
+val = parseInt('12.34')
+val = parseFloat('12.34')
+
+const num1 = '8'
+const num2 = 18
+const sum = num1 + num2
+console.log(sum)
---
+++ 09 numbers/app.js
@@ -0,0 +1,33 @@
+const num1 = 8
+const num2 = 6
+let val
+//Addition
+val = num1 + num2
+//Subtraction
+val = num1 - num2
+//Multiplication
+val = num2 * num1
+//Division
+val = num1 / num2
+//Modulus (Remainder)
+val = num1 % num2
+
+//Math Object
+val = Math.PI
+val = Math.round(8.7)
+val = Math.round(8.4)
+val = Math.pow(8, 2)
+val = Math.sqrt(64)
+val = Math.abs(-4)
+val = Math.ceil(8.1)
+val = Math.floor(8.9)
+val = Math.min(2, 6, 1)
+val = Math.max(2, 6, 1)
+
+//Math.random() returns a random number between 0 (inclusive), and 1 (exclusive):
+val = Math.random()
+//Random between 1 to 5
+val = Math.floor(Math.random() * 5 + 1)
+console.log(val)
+
+
