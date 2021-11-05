1c91e901573d55105695a0c2806ed064a076ab4c
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-11-05 21:19:31 +0500

    Updated

---
+++ 16 date object/app.js
@@ -0,0 +1,6 @@
+let val
+let d = new Date('12-25-2020 11:30:00')
+d = new Date('12/25/2022')
+d = new Date('December 25 2021')
+console.log(d)
+// console.log(val)
---
+++ 17 if statement/app.js
@@ -0,0 +1,34 @@
+let mark = 46
+//Abstract equality comparsion
+// if (mark == 50) {
+//     console.log('The mark is 50')
+// } else {
+//     console.log('The mark is NOT 50')
+// }
+// if (mark != 40) {
+//     console.log('The mark is NOT 40')
+// } else {
+//     console.log('The mark is 40')
+// }
+//Strictly equality comparsion
+// if (mark !== 50) {
+//     console.log('The mark is NOT 50')
+// } else {
+//     console.log('The mark is  50')
+// }
+
+//check undefined
+// if (typeof mark !== 'undefined') {
+//     console.log(`The mark is ${mark}`)
+// } else {
+//     console.log('The mark is undefined')
+// }
+
+//greater than or equal
+//less than
+if (mark < 50) {
+    console.log('You fail the test')
+} else {
+    console.log('You pass the test')
+
+}
