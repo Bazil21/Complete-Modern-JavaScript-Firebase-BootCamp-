39e2559259fe26078cac48455d25254045e9e05b
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-11-05 22:16:34 +0500

    Updated

---
+++ 18 logical operator/app.js
@@ -0,0 +1,52 @@
+//else if
+// let hour = 21
+// if (hour < 12) {
+//     console.log('Good morning!')
+// } else if (hour < 20) {
+//     console.log('How do you do?')
+// } else {
+//     console.log('Good evening')
+// }
+
+//logical operator
+let mark = 23
+
+// AND operator
+// if (mark > 50 && mark <= 80) {
+//     console.log('Good')
+// } else if (mark > 80) {
+//     console.log('Excellent !!')
+// } else {
+//     console.log('Keep it up')
+// }
+
+
+// OR operator
+// let hour = 12;
+// let isWeekend = true
+
+// if (hour < 10 || hour > 17 || isWeekend) {
+//     console.log('The office is now closed')
+// } else {
+//     console.log('The office is now opened')
+// }
+
+
+
+//Ternary operator
+
+let age = 16
+// if (age < 18) {
+//     console.log('You CANNOT drink beer')
+// } else {
+//     console.log('You can drink beer')
+// }
+
+// const drink = age < 18 ? 'You CANNOT drink beer' : 'You can drink beer'
+// console.log(drink)
+
+
+
+
+//one line
+if (age < 18) console.log('You CANNOT drink beer'); else console.log('You can drink beer')
---
+++ 19 switch case/app.js
@@ -0,0 +1,59 @@
+// const fruit = "sdfsdfsd"
+// switch (fruit) {
+//     case "apple":
+// //         console.log('red')
+// //         break;
+// //     case "orange":
+// //         console.log('orange')
+// //         break;
+// //     case "banana":
+// //         console.log('yellow')
+// //         break;
+
+// //     default:
+// //         console.log('I don\'t know')
+// //         break;
+// // }
+
+// let today = new Date().getDay()
+// today = new Date('12/25/2022').getDay()
+// switch (today) {
+//     case 0:
+//         console.log('Sunday')
+//         break;
+//     case 1:
+//         console.log('Monday')
+//         break;
+//     case 2:
+//         console.log('Tuesday')
+//         break;
+//     case 3:
+//         console.log('Wednesday')
+//         break;
+//     case 4:
+//         console.log('Thursday')
+//         break;
+//     case 5:
+//         console.log('Friday')
+//         break;
+//     case 6:
+//         console.log('Saturday')
+//         break;
+
+//     default:
+//         break;
+// }
+
+const x = 22
+switch (true) {
+    case (x < 5):
+        console.log('less than 5')
+        break;
+    case (x < 10):
+        console.log('between 5 and 10')
+        break;
+
+    default:
+        console.log('greater than 10')
+        break;
+}
