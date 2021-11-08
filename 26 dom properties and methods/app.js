152df90168edbaa1abf140325cb0961bce04a139
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-11-08 20:31:17 +0500

    Updated

---
+++ 26 dom properties and methods/app.js
@@ -0,0 +1,11 @@
+let doc
+doc = document
+doc = document.all[1]
+doc = document.head
+doc = document.body
+doc = document.doctype
+doc = document.domain
+doc = document.URL
+doc = document.characterSet
+doc = document.contentType
+console.log(doc)
