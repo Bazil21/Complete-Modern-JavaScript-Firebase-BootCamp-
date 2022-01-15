c45c07fbac413c3f6e576acb724872a93bf5b0c9
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2022-01-16 01:29:23 +0500

    Updated

---
+++ 67 Different types of errors/app.js
@@ -0,0 +1,14 @@
+
+try {
+    //TypeError
+    //SyntaxError
+    //ReferenceError
+    //URIError
+   
+} catch (error) {
+   // console.log(error.message)
+} finally {
+    // console.log('close file')
+}
+
+console.log('continue')
